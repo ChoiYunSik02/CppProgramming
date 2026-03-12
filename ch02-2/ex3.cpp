ce1e9d8082deda62a9ab8a8430ba251d7ecf8d51
ChoiYunSik02 <rudtlr502@gmail.com>
2026-03-12 16:44:47 +0900

    Update ex3.cpp

--- ch02-2/ex3.cpp
+++ ch02-2/ex3.cpp
@@ -1,1 +1,31 @@
+// **********************************************
+// 제 목 : C++ ch02 실습과제3
+// 날 짜 : 2026년 03월 12일
+// 작성자 : 2101091 최윤식
+// **********************************************
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int id;
+    string name, address;
+
+    cout << "학번을 입력하시오: ";
+    cin >> id;
+    cin.ignore(); // 키보드 버퍼에 남아있는 엔터키('\n') 제거
+
+    cout << "이름을 입력하시오: ";
+    getline(cin, name);
+
+    cout << "주소를 입력하시오: ";
+    getline(cin, address);
+
+    cout << endl;
+    cout << "1.학번:" << id << endl;
+    cout << "2.이름:" << name << endl;
+    cout << "3.주소:" << address << endl;
+
+    return 0;
+}
