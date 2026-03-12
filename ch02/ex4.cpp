a299f46ebc278b50690f554bdba37d925243c522
ChoiYunSik02 <rudtlr502@gmail.com>
2026-03-12 16:17:06 +0900

    Update ex4.cpp

--- ch02/ex4.cpp
+++ ch02/ex4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    // X자 모양의 크기 (7x7)
+    int size = 7;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            // 주 대각선(i == j) 이거나, 부 대각선(i + j == size - 1)일 때 별 출력
+            if (i == j || i + j == size - 1) {
+                cout << "*";
+            }
+            else {
+                cout << " "; // 그 외에는 공백 출력
+            }
+        }
+        cout << endl; // 한 행이 끝나면 줄바꿈
+    }
+
+    return 0;
+}
