e31219ca8f1c2224beaf813bdd4e0d49a8a01388
ChoiYunSik02 <rudtlr502@gmail.com>
2026-03-16 14:03:10 +0900

    Update ex2.cpp

--- ch03/ex2.cpp
+++ ch03/ex2.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+class Triangle {
+public:
+	int width;
+	int height;
+	int getArea();
+};
+
+int Triangle::getArea() {
+	return 0.5 * width * height;
+}
+
+int main() {
+	Triangle tra;
+	tra.width = 7;
+	tra.height = 8;
+	cout << "삼각형의 면적은 " << tra.getArea() << endl;
+}
