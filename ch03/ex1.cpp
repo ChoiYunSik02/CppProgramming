b6376ea45d6afe59d51d37f87881bc1a09c3a544
ChoiYunSik02 <rudtlr502@gmail.com>
2026-03-16 13:56:25 +0900

    Update ex1.cpp

--- ch03/ex1.cpp
+++ ch03/ex1.cpp
@@ -1,31 +1,27 @@
 // **********************************************
 // 제 목 : sizeof(클래스명) 활용 코드
-// 날 짜 : 2026년 03월 15일
+// 날 짜 : 2026년 03월 16일
 // 작성자 : 2101091 최윤식
 // **********************************************
 
 #include <iostream>
 using namespace std;
 
-class A {
-    // 멤버 없음
+class Triangle {
+public:
+	int width;
+	int height;
+	int getArea();
 };
 
-class B {
-    int x;        // 4 bytes
-    double y;     // 8 bytes
-};
-
-class C {
-    int x;        // 4 bytes
-    char c;       // 1 byte (+ 패딩 3 bytes)
-    double y;     // 8 bytes
-};
+int Triangle::getArea() {
+	return 0.5 * width * height;
+}
 
 int main() {
-    cout << "sizeof(A) = " << sizeof(A) << " bytes" << endl;
-    cout << "sizeof(B) = " << sizeof(B) << " bytes" << endl;
-    cout << "sizeof(C) = " << sizeof(C) << " bytes" << endl;
-
-    return 0;
+	Triangle tra;
+	tra.width = 7;
+	tra.height = 8;
+	cout << "삼각형의 면적은 " << tra.getArea() << endl;
+	cout << "sizeof의 값은 " << sizeof(Triangle) << endl;
 }
