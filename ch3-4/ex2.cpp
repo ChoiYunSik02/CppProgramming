627be113adea68d85ef80bec5671f772dd1f40ea
ChoiYunSik02 <rudtlr502@gmail.com>
2026-03-24 22:24:24 +0900

    Update ex2.cpp

--- ch3-4/ex2.cpp
+++ ch3-4/ex2.cpp
@@ -24,7 +24,7 @@ int Circle::getRadius() { return radius; } // 멤버 변수 값 읽기
 int main() {
 	Circle waffle1;
 	Circle waffle2(5); // 생성자를 이용한 멤버변수 설정
-	// waffle.radius = 5; // 기존코드
+	// waffle.radius = 5; // 기존코드, 멤버 변수 값 호출 에러 
 	cout << "원의 반지름은 " << waffle1.getRadius() << endl;
 	cout << "원의 반지름은 " << waffle2.getRadius() << endl;
 	return 0;
