a109ef61533d22621071d8547c5c1600e5aad695
ChoiYunSik02 <rudtlr502@gmail.com>
2026-04-13 01:33:50 +0900

    Update ex4-2.cpp

--- ch4-4/ex4-2.cpp
+++ ch4-4/ex4-2.cpp
@@ -1,1 +1,26 @@
+// **********************************************
+// 제 목 : C++ ch04 실습문제 4번 - 4번 (알파벳과 빈칸만 필터링)
+// 날 짜 : 2026년 04월 13일
+// 작성자 : 2101091 최윤식
+// **********************************************
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "텍스트 입력(한글 안 됨)>>";
+    getline(cin, s, '\n');
+
+    string dest = "";
+    for (int i = 0; i < (int)s.length(); i++) {
+        char ch = s[i];
+        if (isalpha(ch) || ch == ' ') {
+            dest += ch;
+        }
+    }
+
+    cout << dest << endl;
+    return 0;
+}
