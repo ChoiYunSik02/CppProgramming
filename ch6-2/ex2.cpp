2c464952fa0b29ecd905592df862d9a3a1e29f8c
ChoiYunSik02 <rudtlr502@gmail.com>
2026-05-17 01:06:44 +0900

    Update ex2.cpp

--- ch6-2/ex2.cpp
+++ ch6-2/ex2.cpp
@@ -1,1 +1,32 @@
+// **********************************************
+// 제 목 : C++ ch06-2 실습과제2 - Rectangle 클래스 (디폴트 매개변수)
+// 날 짜 : 2026년 05월 17일
+// 작성자 : 2101091 최윤식
+// **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+	int width;
+	int height;
+public:
+	Rectangle(int w = 1, int h = 1) { width = w; height = h; }
+	void show();
+};
+
+void Rectangle::show() {
+	cout << "사각형 폭은 " << width << " 높이는 " << height << endl;
+}
+
+int main() {
+	Rectangle rect0;
+	rect0.show();
+	Rectangle rect1(10);
+	rect1.show();
+	Rectangle rect2(10, 20);
+	rect2.show();
+	return 0;
+}
 
